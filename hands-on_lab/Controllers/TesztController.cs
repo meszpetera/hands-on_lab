f327736780bbff9efbe211c8319033da0062dd75
Péter Attila Mészáros <peter.meszaros5@stud.uni-corvinus.hu>
2023-11-28 08:32:35 +0100

    Add project files.

---
+++ hands-on_lab/Controllers/TesztController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace hands_on_lab.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TesztController : ControllerBase
+    {
+    }
+}
